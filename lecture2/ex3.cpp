23734e23fe174c99ca40d662044d8479b5eeda19
мих <95756478+pechikoo11@users.noreply.github.com>
2024-09-30 09:24:20 +0300

    Update and rename ex3 to ex3.cpp

---
+++ lecture2/ex3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+ 
+int main() {
+  int N, S;
+  std::cin >> N >> S;
+  --S;
+  std::vector<std::vector<int>> graph(N);
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      bool path;
+      std::cin >> path;
+      if (path) {
+        graph[i].push_back(j);
+        graph[j].push_back(i);
+      } } //зачем такой ужас..
+  } std::vector<bool> visits(N, false);
+  std::queue<int> q;
+  q.push(S);
+  while (!q.empty()) {
+    int curr = q.front();
+    q.pop();
+    for (int next : graph[curr]) {
+      if (visits[next]) continue;
+      q.push(next);
+      visits[next] = true;
+    } visits[curr] = true;
+  } int count = 0;
+  for (bool visit : visits) {
+    if (visit) ++count;
+  } std::cout << count - 1;
+}
